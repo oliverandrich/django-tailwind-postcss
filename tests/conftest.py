c6ebceba957446d161d340c8fa78c91973a6434a
Oliver Andrich <oliver@andrich.me>
2021-04-10 20:44:42 +0200

    Another tweak to the pytest fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,15 +11,18 @@ from django.core.management import call_command
 @pytest.fixture
 def installed_app(settings):
     app_name = f'test_theme_{str(uuid.uuid4()).replace("-", "_")}'
+    app_path = Path(app_name)
 
     call_command("tailwind", "init", app_name)
+
+    while not app_path.is_dir():
+        time.sleep(1)
+
     settings.INSTALLED_APPS += [app_name]
     settings.TAILWIND_APP_NAME = app_name
 
     app_label = app_name.split(".")[-1]
     app_path = Path(apps.get_app_config(app_label).path)
-
-    time.sleep(1)
 
     yield app_name, app_path
 
